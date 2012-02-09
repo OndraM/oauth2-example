78be925dfdb8e22060909f35e87762cf101932e2
Ondřej Machulda <ondrej.machulda@gmail.com>
2012-02-09 01:54:10 +0100

    Initial settings

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,17 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    protected function _initEnvironment() {
+        mb_internal_encoding('utf-8');
+        date_default_timezone_set('Europe/Prague');
+    }
+
+    protected function _initDoctype() {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('HTML5');
+    }
+
 
 }
 
